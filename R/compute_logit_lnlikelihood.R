d07f482f3fcdcf35718cb52d2407bf7a14c553bb
komatsuna4747 <shota.komatsu@sansan.com>
2021-07-30 11:38:25 +0900

    行列演算で対数尤度を計算するように修正

--- R/compute_logit_lnlikelihood.R
+++ R/compute_logit_lnlikelihood.R
@@ -1,12 +1,9 @@
-compute_logit_lnlikelihood <- function (param, y, X) {
-  output <- 0
-  for (i in 1:length(y)) {
+compute_logit_lnlikelihood <- function (param_constant, param_covariate, y, X) {
+  # Compute exp(xb)
+  exp_xb <- exp(param_constant + X %*% param_covariate)
 
-    exp_xb <- 0
-    for (k in 1:ncol(X)) {
-      exp_xb <- exp_xb + X[i, k] * param[[k]]
-    }
-    output <- output + y[[i]] * log(exp_xb/(1 + exp_xb)) + (1 - y[[i]]) * log(1/(1 + exp_xb))
-  }
+  # Compute logit log-likelihood.
+  # log(L(y, X; \theta)) = \sum y * log(exp(xb) / (1 + exp(xb))) + (1-y) * log(1 / (1 + exp(xb)))
+  output <- sum(y * log(exp_xb/ (1 + exp_xb)) + (1 - y) * log(1 / (1 + exp_xb)))
   return(output)
 }
